fecaa8f55bd36cd315dadd1574b1d00e4328af69
harry_laptop <harrynhj@gmail.com>
2022-09-08 11:18:28 -0500

    update move listener

--- src/main/java/com/walls/wallsplugin/events/Move.java
+++ src/main/java/com/walls/wallsplugin/events/Move.java
@@ -2,6 +2,7 @@ package com.walls.wallsplugin.events;
 
 
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -17,8 +18,15 @@ public class Move implements Listener {
         double from_y = move.getFrom().getY();
         double from_z = move.getFrom().getZ();
 
-        move.getPlayer().sendMessage(ChatColor.GREEN + "from: " + from_x + from_y + from_z);
-        move.getPlayer().sendMessage(ChatColor.GREEN + "to: " + to_x + to_y + to_z);
+        Location loc = move.getPlayer().getLocation();
+
+        move.getPlayer().sendMessage(ChatColor.GREEN + "from: " + Math.round(from_x) + ", " + Math.round(from_y) + ", " + Math.round(from_z));
+        move.getPlayer().sendMessage(ChatColor.GREEN + "to: " + Math.round(to_x) + ", " + Math.round(to_y) +  ", " + Math.round(to_z));
+        if (to_y > 255) {
+            move.getPlayer().teleport(loc);
+            move.getPlayer().sendMessage(ChatColor.RED + "Too high");
+        }
+
 }
 
 }
